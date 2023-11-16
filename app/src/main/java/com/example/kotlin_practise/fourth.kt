e85984b028b9feaef62e54127131578954f0c3a6
amoierov <moierovtracer@gmail.com>
2023-11-16 09:02:46 +0400

    fourthStage

---
+++ app/src/main/java/com/example/kotlin_practise/fourth.kt
@@ -0,0 +1,24 @@
+package com.example.kotlin_practise
+
+fun List<Int>.decorateEvenElements(): List<Int> {
+    return mapIndexed { index, value -> if (index % 2 != 0) value * 2 else value }
+}
+
+interface ListDecorator {
+    fun decorate(list: List<Int>): List<Int>
+}
+
+class EvenElementDoublingDecorator : ListDecorator {
+    override fun decorate(list: List<Int>): List<Int> {
+        return list.mapIndexed { index, value ->
+            if (index % 2 != 0) value * 2 else value }
+    }
+}
+
+fun main() {
+    val numbers = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+    println(numbers.decorateEvenElements().toString())
+    val numbersDoubling = listOf(1, 2, 3, 4, 5, 6, 18, 2931, 2931, 2931, 2931, 2931, 2931)
+    val decorator = EvenElementDoublingDecorator()
+    println(decorator.decorate(numbersDoubling).toString())
+}
